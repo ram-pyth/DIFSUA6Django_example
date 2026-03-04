d95455ffaa6724051a326298198afe23c050a1b0
Ramunas P <ramunas.platukis@gmail.com>
2026-03-04 19:51:05 +0200

    sumodeliuojam savo appui db lentelę

--- projektai_app/models.py
+++ projektai_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Projektas(models.Model):
+    pavadinimas = models.CharField('NT_projektas', max_length=40)
+    plotas = models.FloatField('Plotas kv m')
+    kaina = models.FloatField('Kaina be PVM')
+    ivedimo_data = models.DateTimeField('Įved. data', default=datetime.now)
+
+    def __str__(self):
+        return f"{self.pavadinimas} {self.plotas} {self.kaina} {self.ivedimo_data}"
+
+    @property
+    def kaina_pvm(self):
+        return round(self.kaina * 1.21, 2)
+
+# !!!!! parašius naują modelio klasę, visada yra vykdomos migracijos
+# python manage.py makemigrations
+# python manage.py migrate
